7261982523ce55df9449f4058afcd3ec5331d3ac
junxuchen <753720325@qq.com>
2022-04-17 10:13:00 +0800

    Changes

--- get_data.py
+++ get_data.py
@@ -1,16 +1,28 @@
+import configparser
+import datetime
+import logging
+
 import baostock as bs
 import pandas as pd
-import datetime
 from mysql import connector
 
 
-def get_daily_info(self):
-    connect = connector.connect(host='rm-wz96dr9a71u1pc0wono.mysql.rds.aliyuncs.com',
-                                user='junxuchen', passwd='Cjx@123456', db='analyze-python')
+def get_daily_info():
+    begin_time = datetime.datetime.now()
+    log_format = "%(asctime)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_format)
+    cp = configparser.ConfigParser()
+    cp.read('configuration_local.ini', encoding='UTF-8')
+    # cp.read('configuration.ini', encoding='UTF-8')
+    mysql_config = dict(cp.items('mysql'))
+    connect = connector.connect(host=mysql_config["host"],
+                                user=mysql_config["user"],
+                                passwd=mysql_config["passwd"],
+                                db=mysql_config["db"],
+                                port=mysql_config["port"])
     cursor = connect.cursor()
-    # 登陆系统\
+    # 登陆系统
     lg = bs.login()
-    # current_date = '2022-04-15'
     current_date = datetime.date.today().__str__()
     # 显示登陆返回信息
     print('login respond error_code:' + lg.error_code)
@@ -19,21 +31,25 @@ def get_daily_info(self):
     # 详细指标参数，参见“历史行情指标参数”章节；“分钟线”参数与“日线”参数不同。“分钟线”不包含指数。
     # 分钟线指标：date,time,code,open,high,low,close,volume,amount,adjustflag
     # 周月线指标：date,code,open,high,low,close,volume,amount,adjustflag,turn,pctChg
-    insert_sql = 'INSERT INTO `t_securities_daily`( `securities_id` , `day` , `begin` , `height` , `low` , `end` , ' \
+    insert_sql = 'INSERT INTO `t_securities_daily`( `securities_id` , `day` , `begin` , `high` , `low` , `end` , ' \
                  '`pre_end` , `turnover_rate` , `is_top` , `is_last` , `turnover` , `turnover_number` , `is_st` , ' \
-                 '`percent` , `pe_ttm` ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+                 '`percent` , `pe_ttm`, `trade_status` ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, ' \
+                 '%s, %s, %s) '
     cursor.execute("select id,code,market from t_securities where market in ('sh','sz')")
     code_list = cursor.fetchall()
+    error_count = 0
+    insert_count = 0
     for code_item in code_list:
         try:
             code = code_item[2] + '.' + code_item[1]
             rs = bs.query_history_k_data_plus(code,
                                               "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,"
                                               "tradestatus,pctChg,isST,peTTM",
-                                              start_date='2022-04-14', end_date=current_date,
+                                              start_date='2022-01-01', end_date=current_date,
                                               frequency="d", adjustflag="3")
-            print('query_history_k_data_plus respond error_code:' + rs.error_code)
-            print('query_history_k_data_plus respond  error_msg:' + rs.error_msg)
+            if rs.error_code != '0':
+                print('query_history_k_data_plus respond error_code:' + rs.error_code)
+                print('query_history_k_data_plus respond error_msg:' + rs.error_msg)
 
             # 打印结果集 ####
             data_list = []
@@ -44,23 +60,55 @@ def get_daily_info(self):
             if len(data_list) > 0:
                 item_list = []
                 for index in range(len(result.get('code'))):
-                    item = (code_item[0], current_date, result.get('open').values[0],
-                            result.get('high').values[0], result.get('low').values[0], result.get('close').values[0],
-                            result.get('preclose').values[0], result.get('turn').values[0],
-                            result.get('close').values[0] == result.get('high').values[0],
-                            result.get('close').values[0] == result.get('low').values[0],
-                            result.get('volume').values[0], result.get('amount').values[0],
-                            result.get('isST').values[0] == '1', result.get('pctChg').values[0],
-                            result.get('peTTM').values[0])
-                    item_list.append(item)
+                    data = {"securities_id": code_item[0],
+                            "day": result.get('date').values[index],
+                            "begin": result.get('open').values[index],
+                            "high": result.get('high').values[index],
+                            "low": result.get('low').values[index],
+                            "end": result.get('close').values[index],
+                            "pre_end": result.get('preclose').values[index],
+                            "turnover_rate": result.get('turn').values[index],
+                            "is_top": result.get('close').values[index] == result.get('high').values[index],
+                            "is_last": result.get('close').values[index] == result.get('low').values[index],
+                            "turnover": result.get('volume').values[index],
+                            "turnover_number": result.get('amount').values[index],
+                            "is_st": result.get('isST').values[index] == '1',
+                            "percent": result.get('pctChg').values[index],
+                            "pe_ttm": result.get('peTTM').values[index],
+                            "trade_status": result.get('tradestatus').values[index] == '1'}
+                    item_list.append(build_item(data))
+                insert_count += len(item_list)
                 cursor.executemany(insert_sql, item_list)
-                print('insert item: ', item_list)
+                connect.commit()
+                logging.info('insert item: %s', item_list)
         except BaseException as e:
-            print('insert daily info error', e)
-
+            error_count += 1
+            logging.error('insert daily info error %s', e)
+            logging.error(e)
+    logging.info('insert: %s, error: %s, time: %s', insert_count, error_count, datetime.datetime.now() - begin_time)
     # 登出系统 ####
     bs.logout()
 
 
+def build_item(data: dict):
+    item = (data["securities_id"],
+            data["day"],
+            data["begin"],
+            data["high"],
+            data["low"],
+            data["end"],
+            data["pre_end"],
+            0.0 if data["turnover_rate"] == '' else data["turnover_rate"],
+            data["is_top"],
+            data["is_last"],
+            0.0 if data["turnover"] == '' else data["turnover"],
+            0.0 if data["turnover_number"] == '' else data["turnover_number"],
+            data["is_st"],
+            0.0 if data["percent"] == '' else data["percent"],
+            data["pe_ttm"],
+            data["trade_status"])
+    return item
+
+
 if __name__ == '__main__':
-    get_daily_info(None)
+    get_daily_info()
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+import configparser
 import json
 import math
+
 import requests
 from mysql import connector
 
 # 通过接口获取券商代码
 if __name__ == '__main__':
-    connect = connector.connect(host='rm-wz96dr9a71u1pc0wono.mysql.rds.aliyuncs.com',
-                                user='junxuchen', passwd='Cjx@123456', db='analyze-python')
+    cp = configparser.ConfigParser()
+    cp.read('configuration_local.ini', encoding='UTF-8')
+    # cp.read('configuration.ini', encoding='UTF-8')
+    mysql_config = dict(cp.items('mysql'))
+    connect = connector.connect(host=mysql_config["host"],
+                                user=mysql_config["user"],
+                                passwd=mysql_config["passwd"],
+                                db=mysql_config["db"],
+                                port=mysql_config["port"])
     cursor = connect.cursor()
+    # 如新增股需要修改如下总数值，具体可由接口获知
     page_total = math.ceil(4954 / 20)
     index = 1
+    # 该接口非永久接口，具体接口见东方财富-行情中心-个股行情
+    url = 'http://92.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112407958538666454928_1650094040204&' \
+          'pn={page}&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,' \
+          'm:0+t:80,m:1+t:2,m:1+t:23,m:0+t:81+s:2048&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,' \
+          'f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1650094040205'
     while index <= page_total:
-        url = 'http://92.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112407958538666454928_1650094040204&' \
-              'pn={page}&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,' \
-              'm:0+t:80,m:1+t:2,m:1+t:23,m:0+t:81+s:2048&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,' \
-              'f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1650094040205'.format(page=index)
-        response = requests.get(url)
+        get_url = url.format(page=index)
+        response = requests.get(get_url)
         index = index + 1
         data = response.text
         str_test = json.loads(data[data.index('(') + 1:data.rindex(')')])
@@ -37,5 +49,3 @@ if __name__ == '__main__':
             cursor.execute('insert into `t_securities` (`code`, `name`, `market`) values (%s, %s, %s)', item)
             print('insert item: ', item)
             connect.commit()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
